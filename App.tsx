2881dc5f1f6508cf166ee0a114abbc264040fd09
ruandqm <ruandqm@gmail.com>
2023-06-25 15:29:19 -0300

    initial commit

--- App.tsx
+++ App.tsx
@@ -1,20 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Platform, SafeAreaView, StatusBar, StyleSheet, Switch, Text, View } from 'react-native';
+import { myColors } from './src/styles/Colors';
+import { ThemeContext } from './src/context/ThemeContext';
+import MyKeyboard from './src/components/MyKeyboard';
+
 
 export default function App() {
+  //Developed with study purposes by Ruan de Queiroz
+
+  const [theme, setTheme] = useState('light');
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ThemeContext.Provider value={theme}>
+      <StatusBar backgroundColor={myColors.dark} />
+      <SafeAreaView style={theme === 'light' ? styles.container : [styles.container, { backgroundColor: 'black' }]}>
+
+        <View
+          style={{
+            flexDirection: 'row', alignItems: 'center'
+          }}>
+
+          <Text style={{ color: myColors.gray }}>Alterar Tema</Text>
+          <Switch
+            value={theme === 'dark'}
+            onValueChange={() => setTheme(theme === 'light' ? 'dark' : 'light')}
+          />
+        </View>
+
+        <MyKeyboard />
+      </SafeAreaView>
+    </ThemeContext.Provider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: myColors.light,
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0
   },
 });
---
+++ README.md
@@ -0,0 +1,29 @@
+# React Native Calculator
+
+
+![Simulator Screen Shot - Google Pixel](https://lh3.googleusercontent.com/pw/AJFCJaWtIR7w7jF0v4jwjcbkv9Cs9A9f0murjOqXgZyTLSsqSQ3C8SsB3XpI4CLcQmVL6V6WMokFM5FIdWZuk0HfFtqG1d7D4Cq7uVf1KSmHBuWKij6-Bo_vmojQTGbBh8QnePwEhzKAFMCU4RQoaiLNX2m5iA=w327-h632-s-no)
+![Simulator Screen Shot - Google Pixel](https://lh3.googleusercontent.com/pw/AJFCJaVRbnopHIJG4Z3FRmizL35jgtJI0ly0GUzhoQJh7juez4xNsKddXw69Zdnhabux6G1YkPrfE0k8OeTBEqGVrUnAWGzaDOGKkXwjmdSZ8cy11inL9Qf5lpR_Y8OTsDCGxqBbbXToy6vEiRbJoVJFLtnZkw=w334-h627-s-no)
+
+### About
+This is a calculator app using React Native to explore its functionalities and enhance my understanding of the framework. The calculator features a user-friendly interface with a switcher that allows users to toggle between dark mode and light mode.  With this calculator, users can perform basic arithmetic operations and experience a visually appealing interface that adapts to their preferred mode.
+
+By developing this calculator project, I aimed to familiarize myself with React Native's components, state management, and styling capabilities.
+
+### Features
+- Dark mode
+- Beautiful UI
+- Minimalistic
+
+### Tools
+- React Native
+- Typescript
+
+### How to run this project
+At the root folder run:
+```
+yarn && yarn start
+```
+or, if you prefer:
+```
+npm install && npm start
+```
